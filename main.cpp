05a1510848ec2ff9273f55d020d317602ead6799
andrewpeterkort <andrew.j.peterkort@gmail.com>
2019-02-19 20:11:53 -0800

    Fixed buts in file scan. Loads constants into 2D array

--- main.cpp
+++ main.cpp
@@ -5,28 +5,59 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <sstream>
+
+void printConst(int** constArray){
+
+    for(int i = 0; i < 6; i++){
+
+        for(int j = 0; j < 6; j++){
+            
+            std::cout << constArray[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
 
 int** loadConst(){
 
+    int lineNumber = 0;
+    std::string line, token;
     std::fstream constFile;
+    char** ptr = nullptr;
     constFile.open("imp2cost.txt", std::ios::in);
 
-    int** costArray = new int*[5];
-    for(int i = 0; i < 5; i++)
-        costArray[i] = new int[5];
-        
-    std::string line;
-    while(std::getline(constFile, line))
-        std::cout << line << std::endl;
+    int** constArray = new int*[6];
+    for(int i = 0; i < 6; i++)
+        constArray[i] = new int[6];
 
-    return costArray;
+    for(int i = 0; i < 6; i++){
+
+        std::getline(constFile, line);
+        std::stringstream stream(line);
+
+        for(int j = 0; j < 6; j++){
+
+            std::getline(stream, token, ',');
+            constArray[lineNumber][j] = strtol(token.c_str(), ptr, 10);
+        }
+
+        lineNumber++;
+    }
+
+    constFile.close();
+    return constArray;
 }
-
-
 
 int main(){
 
-    loadConst();
+    int** constArray = loadConst();
+    printConst(constArray);
 
+    for(int i = 0; i < 6; i++)
+        delete[] constArray[i];
+
+    delete[] constArray;
     return 0;
 }
